e5b4216b8d8f030d774bead48746585d20bf0802
Gijs Mulder <gijsml1167@gmail.com>
2016-11-05 16:59:49 +0100

    Initial commit

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -1,5 +1,8 @@
 package org.devathon.contest2016;
 
+import org.bukkit.Instrument;
+import org.bukkit.Note;
+import org.bukkit.Sound;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DevathonPlugin extends JavaPlugin {
