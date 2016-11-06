5f1cb7c4651c78de56afcc704ce7d342c62d002e
Gijs Mulder <gijsml1167@gmail.com>
2016-11-06 14:00:15 +0100

    Fixed bug

--- src/main/java/org/devathon/contest2016/listeners/BlockRedstoneListener.java
+++ src/main/java/org/devathon/contest2016/listeners/BlockRedstoneListener.java
@@ -7,17 +7,22 @@ import org.bukkit.event.block.BlockPhysicsEvent;
 import org.devathon.contest2016.DataLoader;
 import org.devathon.contest2016.musicbox.MusicBox;
 
+import java.io.IOException;
+
 /**
  * Created by Gijs on 6-11-2016.
  */
 public class BlockRedstoneListener implements Listener{
 
     @EventHandler
-    public void onRedstone(BlockPhysicsEvent event) {
+    public void onRedstone(BlockPhysicsEvent event) throws IOException {
         if (event.getBlock().getType() != Material.ENDER_PORTAL_FRAME) return;
         if (DataLoader.dataMap.containsKey(event.getBlock())) {
             MusicBox box = DataLoader.loadBox(event.getBlock());
-            box.data.on = event.getBlock().getBlockPower() > 0;
+            boolean on = event.getBlock().getBlockPower() > 0;
+            boolean toggle =  box.data.on != on;
+            box.data.on = on;
+            if (toggle) DataLoader.saveBox(box);
         }
     }
 }
